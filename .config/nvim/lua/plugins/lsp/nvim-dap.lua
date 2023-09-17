f4c1f4cd8e7e25112ede173c37feebe7d9c51695
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-09-17 04:53:02 -0400

    dapui setup

--- .config/nvim/lua/plugins/lsp/nvim-dap.lua
+++ .config/nvim/lua/plugins/lsp/nvim-dap.lua
@@ -5,11 +5,16 @@ return {
 	dependencies = {
 		"jay-babu/mason-nvim-dap.nvim",
 		"williamboman/mason.nvim",
-		'nvim-treesitter/nvim-treesitter',
-		'theHamsta/nvim-dap-virtual-text',
+		"nvim-treesitter/nvim-treesitter",
+		"theHamsta/nvim-dap-virtual-text",
+		"rcarriga/nvim-dap-ui",
 	},
 	config = function ()
 		local mason_dap = require("mason-nvim-dap")
+		local dap, dapui = require("dap"), require("dapui")
+		dapui.setup()
+		dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+		dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
 		mason_dap.setup({
 			automatic_installation = true,
 			handlers = {
