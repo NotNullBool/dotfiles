b12ec3999039a7f1e65bb79b4bb62c6c11ec05fe
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-09-17 15:35:15 -0400

    testing to see if I could get dressing to work with dap. Didn't work but when it does it will load only on call.

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -1,4 +1,14 @@
 return {
-    -- "stevearc/dressing.nvim",
-    -- event = "VeryLazy",
+    --[[ "stevearc/dressing.nvim",
+	lazy = true,
+	init = function ()
+		vim.ui.select = function (...)
+			require("lazy").load({ plugins = {"dressing.nvim"} })
+			return vim.ui.select(...)
+		end
+		vim.ui.input = function(...)
+			require("lazy").load({ plugins = { "dressing.nvim" } })
+			return vim.ui.input(...)
+		end
+	end ]]
 }
