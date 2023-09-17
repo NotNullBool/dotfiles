467da363451694a5117e934f3e6dcd823108fc32
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-09-17 15:52:20 -0400

    added an empty opt to buffline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,9 +2,9 @@ return {
 	'akinsho/bufferline.nvim',
 	version = "*",
 	dependencies = 'nvim-tree/nvim-web-devicons',
-	config = function ()
+	opts = {},
+	config = function (_, opts)
 	    vim.opt.termguicolors = true
-	    require("bufferline").setup({
-		})
+	    require("bufferline").setup(opts)
 	end
 }
