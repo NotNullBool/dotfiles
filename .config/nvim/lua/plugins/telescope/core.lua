844c2ea674fa0ddd72eed3efcc7d7aeaab44837a
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-09-27 18:35:43 -0400

    moved telescope extension to correct position

--- .config/nvim/lua/plugins/telescope/core.lua
+++ .config/nvim/lua/plugins/telescope/core.lua
@@ -16,6 +16,8 @@ return {
 		local builtin = require("telescope.builtin")
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
 		telescope.setup({
 			defaults = {
@@ -26,53 +28,58 @@ return {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-					}
-				},
-				extensions = {
-					lazy = {
-						-- Optional theme (the extension doesn't set a default theme)
-						-- theme = "ivy",
-						-- Whether or not to show the icon in the first column
-						show_icon = true,
-						-- Mappings for the actions
-						mappings = {
-							open_in_browser = "<C-o>",
-							open_in_file_browser = "<M-b>",
-							open_in_find_files = "<C-f>",
-							open_in_live_grep = "<C-g>",
-							open_plugins_picker = "<C-b>", -- Works only after having called first another action
-							open_lazy_root_find_files = "<C-r>f",
-							open_lazy_root_live_grep = "<C-r>g",
-						},
-						-- Other telescope configuration options
-					},
-					undo = {
-						side_by_side = true,
-						layout_strategy = "vertical",
-						layout_config = {
-							preview_height = 0.8,
-						},
 					},
 				},
-			}})
+			},
+			extensions = {
+				file_browser = {
+					hijack_netrw = true,
+					theme = "ivy"
+				},
+				lazy = {
+					-- Optional theme (the extension doesn't set a default theme)
+					-- theme = "ivy",
+					-- Whether or not to show the icon in the first column
+					show_icon = true,
+					-- Mappings for the actions
+					mappings = {
+						open_in_browser = "<C-o>",
+						open_in_file_browser = "<M-b>",
+						open_in_find_files = "<C-f>",
+						open_in_live_grep = "<C-g>",
+						open_plugins_picker = "<C-b>", -- Works only after having called first another action
+						open_lazy_root_find_files = "<C-r>f",
+						open_lazy_root_live_grep = "<C-r>g",
+					},
+					-- Other telescope configuration options
+				},
+				undo = {
+					side_by_side = true,
+					layout_strategy = "vertical",
+					layout_config = {
+						preview_height = 0.8,
+					},
+				},
+			},
+		})
 
-			local curdir = "plugins.telescope."
-			require(curdir.."highlights").setup()
-			telescope.load_extension("fzf")
-			telescope.load_extension("aerial")
-			telescope.load_extension("lazy")
-			telescope.load_extension("undo")
-			telescope.load_extension("ui-select")
-			telescope.load_extension("file_browser")
-			local keymap = vim.keymap
-			keymap.set('n', "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-			keymap.set('n', "<leader>fg", builtin.live_grep, { desc = "Find string in cwd" })
-			keymap.set('n', "<leader>fb", builtin.buffers, { desc = "Find buffer" })
-			keymap.set('n', "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
-			keymap.set('n', "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-			keymap.set('n', "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-			keymap.set('n', "<leader>ft", "<cmd>Telescope aerial<cr>", { desc = "Fuzzy find functions in file" })
-			keymap.set('n', "<leader>u", "<cmd>Telescope undo<cr>", { desc = "Undo telescope" })
-			keymap.set('n', "<leader>e", "<cmd>Telescope file_browser<cr>", { desc = "Open Telescope file broswer" })
-		end
-	}
+					local curdir = "plugins.telescope."
+					require(curdir.."highlights").setup()
+					telescope.load_extension("fzf")
+					telescope.load_extension("aerial")
+					telescope.load_extension("lazy")
+					telescope.load_extension("undo")
+					telescope.load_extension("ui-select")
+					telescope.load_extension("file_browser")
+					local keymap = vim.keymap
+					keymap.set('n', "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+					keymap.set('n', "<leader>fg", builtin.live_grep, { desc = "Find string in cwd" })
+					keymap.set('n', "<leader>fb", builtin.buffers, { desc = "Find buffer" })
+					keymap.set('n', "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
+					keymap.set('n', "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+					keymap.set('n', "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+					keymap.set('n', "<leader>ft", "<cmd>Telescope aerial<cr>", { desc = "Fuzzy find functions in file" })
+					keymap.set('n', "<leader>u", "<cmd>Telescope undo<cr>", { desc = "Undo telescope" })
+					keymap.set('n', "<leader>e", "<cmd>Telescope file_browser<cr>", { desc = "Open Telescope file broswer" })
+				end
+			}
