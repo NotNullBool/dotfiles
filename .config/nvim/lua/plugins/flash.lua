52a6cf54ca67cd6972e316e665a3b0aa4e9c69c6
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-09-29 14:42:47 -0400

    disable toggle flash search added parenthese for what plugin in the description

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -4,10 +4,10 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "<leader>s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<leader>S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "<leader>r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "<leader>R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<leader>s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Search (Flash)" },
+    { "<leader>S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Search node Treesitter (Flash)" },
+    { "<leader>r", mode = "o", function() require("flash").remote() end, desc = "Remote Search (Flash)" },
+    { "<leader>R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search (Flash)" },
+    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle (Flash) Search" },
   },
 }
