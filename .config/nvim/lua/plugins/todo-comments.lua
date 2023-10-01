e2aa560d33626886d27eaf1c24ea0b06ad59802a
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-01 06:42:59 -0400

    todo-comments event set

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,10 @@
 return {
 	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
 	opts = {
 		keywords = {
 			TEST = {
