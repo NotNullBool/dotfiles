244e8d269a30148f33ac29329f1112b21446d930
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-01 06:46:08 -0400

    lsp-progress set to lazy

--- .config/nvim/lua/plugins/lsp-progress.lua
+++ .config/nvim/lua/plugins/lsp-progress.lua
@@ -1,7 +1,6 @@
 return {
 	'linrongbin16/lsp-progress.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-		require('lsp-progress').setup()
-	end
+	lazy = true,
+	config = true
 }
