c408ea924d2693fb2fe88b19d72b47f141fc12bb
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-01 14:36:21 -0400

    decided tabbing was annoying to move and complete so decided to only allow for snippets

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -28,11 +28,9 @@ return{
 
 
 			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-					-- they way you will only jump inside the snippet region
-				elseif luasnip.expand_or_jumpable() then
+				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+				-- they way you will only jump inside the snippet region
+				if luasnip.expand_or_jumpable() then
 					luasnip.expand_or_jump()
 				else
 					fallback()
@@ -40,9 +38,7 @@ return{
 			end, { "i", "s" }),
 
 			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
+				if luasnip.jumpable(-1) then
 					luasnip.jump(-1)
 				else
 					fallback()
