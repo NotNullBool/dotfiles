6dce683071b9637f4f8cdc7899f9d6d93513d988
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-05 12:09:50 -0400

    increased treesj symbol limit added filetype specific key mappings for nvim-various-textobjs

--- .config/nvim/lua/plugins/nvim-various-textobjs.lua
+++ .config/nvim/lua/plugins/nvim-various-textobjs.lua
@@ -46,5 +46,35 @@ return {
 
 		{"gw", "<cmd>lua require('various-textobjs').visibleInWindow()<CR>", mode = { "o", "x" }},
 		{"gW", "<cmd>lua require('various-textobjs').restOfWindow()<CR>", mode = { "o", "x" }},
+
+		{ "il", "<cmd>lua require('various-textobjs').mdlink('inner')<CR>",
+			mode = { "o", "x" }, ft = {"markdown", "toml"}},
+		{ "al", "<cmd>lua require('various-textobjs').mdlink('outer')<CR>",
+			mode = { "o", "x" }, ft = {"markdown", "toml"}},
+
+		{ "iC", "<cmd>lua require('various-textobjs').mdFencedCodeBlock('inner')<CR>",
+			mode = { "o", "x" }, ft = {"markdown"}},
+		{ "aC", "<cmd>lua require('various-textobjs').mdFencedCodeBlock('outer')<CR>",
+			mode = { "o", "x" }, ft = {"markdown"}},
+
+		{ "ic", "<cmd>lua require('various-textobjs').cssSelector('inner')<CR>",
+			mode = { "o", "x" }, ft = {"css", "scss"}},
+		{ "ac", "<cmd>lua require('various-textobjs').cssSelector('outer')<CR>",
+			mode = { "o", "x" }, ft = {"css", "scss"}},
+
+		{ "ix", "<cmd>lua require('various-textobjs').htmlAttribute('inner')<CR>",
+			mode = { "o", "x" }, ft = {"css", "scss", "html", "xml", "vue"}},
+		{ "ax", "<cmd>lua require('various-textobjs').htmlAttribute('outer')<CR>",
+			mode = { "o", "x" }, ft = {"css", "scss", "html", "xml", "vue"}},
+
+		{ "iD", "<cmd>lua require('various-textobjs').doubleSquareBrackets('inner')<CR>",
+			mode = { "o", "x" }, ft = { "lua", "norg", "sh", "fish", "zsh", "bash", "markdown" }},
+		{ "aD", "<cmd>lua require('various-textobjs').doubleSquareBrackets('outer')<CR>",
+			mode = { "o", "x" }, ft = { "lua", "norg", "sh", "fish", "zsh", "bash", "markdown" }},
+
+		{ "iP", "<cmd>lua require('various-textobjs').shellPipe('inner')<CR>",
+			mode = { "o", "x" }, ft = { "sh", "bash", "zsh", "fish" }},
+		{ "aP", "<cmd>lua require('various-textobjs').shellPipe('outer')<CR>",
+			mode = { "o", "x" }, ft = { "sh", "bash", "zsh", "fish" }}
 	}
 }
