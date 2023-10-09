7e269fcb09c61981571802bac7452dab9b192314
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-09 19:31:32 -0400

    added comment to presence so I dont forget

--- .config/nvim/lua/plugins/presence.lua
+++ .config/nvim/lua/plugins/presence.lua
@@ -2,6 +2,7 @@ return {
 	"andweeb/presence.nvim",
 	event = "VeryLazy",
 	opts = function()
+		-- Create hard link to discord rich presence on startup
 		vim.cmd[[silent! !ln -f $XDG_RUNTIME_DIR/{app/com.discordapp.Discord,}/discord-ipc-0]]
 		-- return {log_level = "debug"}
 	end
