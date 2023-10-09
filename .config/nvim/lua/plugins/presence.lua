3513823b3d05258f0c42e81b186ee4056832abce
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-09 18:51:38 -0400

    since I use discord flatpak and discord sym links refuse to work I start up presence with a hard link so discord will be forced to recognize it

--- .config/nvim/lua/plugins/presence.lua
+++ .config/nvim/lua/plugins/presence.lua
@@ -1,5 +1,8 @@
 return {
 	"andweeb/presence.nvim",
 	event = "VeryLazy",
-	-- opts = {log_level = "debug"}
+	opts = function()
+		vim.cmd[[silent! !ln -f $XDG_RUNTIME_DIR/{app/com.discordapp.Discord,}/discord-ipc-0]]
+		-- return {log_level = "debug"}
+	end
 }
