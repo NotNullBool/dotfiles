bf173e2fd5707962d0dfdb4698d8f723e605289a
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-10 16:12:59 -0400

    moved config to opt for bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,9 +2,8 @@ return {
 	'akinsho/bufferline.nvim',
 	version = "*",
 	dependencies = 'nvim-tree/nvim-web-devicons',
-	opts = {},
-	config = function (_, opts)
-	    vim.opt.termguicolors = true
-	    require("bufferline").setup(opts)
-	end
+	opts = function ()
+		vim.opt.termguicolors = true
+		return {}
+	end,
 }
