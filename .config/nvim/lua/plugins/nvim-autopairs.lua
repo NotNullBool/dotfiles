b1e510f3201cf9dd3e9279e97d23940c4279ed50
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-17 05:35:37 -0400

    changing options for autopairs in quotes to cause more expected behaviour

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,5 @@
 return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = { enable_moveright = false, map_bs = false }
+    opts = { enable_moveright = false, map_bs = false, enable_afterquote= false, enable_bracket_in_quote = false }
 }
